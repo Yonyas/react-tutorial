d4f4e73cd5ca1953c9439b951544355c2f53f23d
Jiyon Lee <jiyonlee.d@gmail.com>
2021-01-29 20:48:19 +0900

    hooks로 변경

--- 숫자맞히기/GuessNumber.jsx
+++ 숫자맞히기/GuessNumber.jsx
@@ -1,4 +1,5 @@
 const React = require("react");
+const { useState } = React;
 const Try = require("./Try");
 
 //이 함수는 this 안써서 밖에 뺄 수 있었다. 제로초는 this 안쓰면 밖에 뺸다. class 안에 넣어도 된다.
@@ -15,30 +16,34 @@ function getNumbers() {
   return array;
 }
 
-class GuessNumber extends React.Component {
-  state = {
-    result: "",
-    value: "",
-    answer: getNumbers(),
-    tries: [],
-  };
+const GuessNumber = () => {
+  const [result, setResult] = useState("");
+  const [value, setValue] = useState("");
+  const [answer, setAnswer] = useState(getNumbers());
+  const [tries, setTries] = useState([]);
 
-  onSubmitForm = (e) => {
-    const { answer, value, tries } = this.state;
+  const onSubmitForm = (e) => {
     e.preventDefault();
     //맞췄으면
     if (value === answer.join("")) {
-      this.setState({
-        result: "홈런!",
-        tries: [...tries, { try: value, result: "홈런!" }],
+      // this.setState((prevState) => {
+      //   return {
+      //     result: "홈런!",
+      //     tries: [...prevState.tries, { try: value, result: "홈런!" }],
+      //   };
+      // });
+      setResult("홈런!");
+      setTries((prevTries) => {
+        return [...prevTries, { try: value, result: "홈런!" }];
       });
+
       alert("게임을 다시 시작합니다.");
-      this.setState({
-        value: "",
-        answer: getNumbers(),
-        tries: [],
-        result: "",
-      });
+
+      setValue("");
+      setAnswer(getNumbers());
+      setTries([]);
+      setResult("");
+
       //틀렸으면
     } else {
       let strike = 0;
@@ -47,16 +52,12 @@ class GuessNumber extends React.Component {
       const myAnswerArray = value.split("").map((v) => parseInt(v));
       //10번 이상 틀렸을 때
       if (tries.length >= 9) {
-        this.setState({
-          result: `10번 넘게 틀려서 실패! 답은 ${answer.join(",")}였습니다.`,
-        });
+        setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(",")}였습니다.`);
         alert("게임을 다시 시작합니다.");
-        this.setState({
-          value: "",
-          answer: getNumbers(),
-          tries: [],
-          result: "",
-        });
+        setValue("");
+        setAnswer(getNumbers());
+        setTries([]);
+        setResult("");
       } else {
         for (let i = 0; i < 4; i++) {
           if (myAnswerArray[i] === answer[i]) {
@@ -65,50 +66,57 @@ class GuessNumber extends React.Component {
             ball++;
           }
         }
-        this.setState({
-          tries: [
-            ...tries,
+        // this.setState((prevState) => {
+        //   return {
+        //     tries: [
+        //       ...prevState.tries,
+        //       {
+        //         try: value,
+        //         result: `${strike} 스트라이크, ${ball} 볼입니다.`,
+        //       },
+        //     ],
+        //     value: "",
+        //   };
+        // });
+
+        setTries((prevTries) => {
+          return [
+            ...prevTries,
             {
               try: value,
               result: `${strike} 스트라이크, ${ball} 볼입니다.`,
             },
-          ],
-          value: "",
+          ];
         });
+        setValue("");
       }
     }
   };
 
-  onChangeInput = (e) => {
-    console.log(this.state.answer);
-    this.setState({
-      value: e.target.value,
-    });
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
   };
 
-  render() {
-    const { result, tries, value } = this.state;
-    return (
-      <>
-        <h1>{result}</h1>
-        <form onSubmit={this.onSubmitForm}>
-          <input
-            type="number"
-            maxLength={4}
-            onChange={this.onChangeInput}
-            value={value}
-          />
-          <button type="submit">입력!</button>
-        </form>
-        <div>시도 횟수: {tries.length}</div>
-        <ul>
-          {tries.map((v, i) => {
-            return <Try key={`${i + 1}차시도: `} tryInfo={v} index={i} />;
-          })}
-        </ul>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1>{result}</h1>
+      <form onSubmit={onSubmitForm}>
+        <input
+          type="number"
+          maxLength={4}
+          onChange={onChangeInput}
+          value={value}
+        />
+        <button type="submit">입력!</button>
+      </form>
+      <div>시도 횟수: {tries.length}</div>
+      <ul>
+        {tries.map((v, i) => {
+          return <Try key={`${i + 1}차시도: `} tryInfo={v} index={i} />;
+        })}
+      </ul>
+    </>
+  );
+};
 
 module.exports = GuessNumber;
--- 숫자맞히기/Try.jsx
+++ 숫자맞히기/Try.jsx
@@ -1,13 +1,22 @@
 const React = require("react");
 
-class Try extends React.Component {
-  render() {
-    return (
-      <li key={this.props.tryInfo.try + this.props.tryInfo.result}>
-        <b>{this.props.tryInfo.try}</b> - {this.props.tryInfo.result}
-      </li>
-    );
-  }
-}
+// class Try extends React.Component {
+//   render() {
+//     const { tryInfo } = this.props;
+//     return (
+//       <li key={tryInfo.try + tryInfo.result}>
+//         <b>{tryInfo.try}</b> - {tryInfo.result}
+//       </li>
+//     );
+//   }
+// }
+
+const Try = ({ tryInfo }) => {
+  return (
+    <li key={tryInfo.try + tryInfo.result}>
+      <b>{tryInfo.try}</b> - {tryInfo.result}
+    </li>
+  );
+};
 
 module.exports = Try;
