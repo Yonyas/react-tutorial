507cce3171d8a758c314531fa81665013559f6d0
Jiyon Lee <jiyonlee.d@gmail.com>
2021-02-01 11:52:17 +0900

    고차함수패턴, setTimeout 적용

--- 가위바위보/RockScissorsPaper.jsx
+++ 가위바위보/RockScissorsPaper.jsx
@@ -24,7 +24,7 @@ class RockScissorsPaper extends Component {
 
   componentDidMount() {
     // 첫 렌더링된 후,, 여기에 비동기 요청을 많이 함.
-    this.interval = setInterval(this.changeHand, 2000);
+    this.interval = setInterval(this.changeHand, 500);
   }
   componentDidUpdate() {
     // 리렌더링 후
@@ -54,7 +54,7 @@ class RockScissorsPaper extends Component {
     return Object.entries(rspCoord).find((v) => v[1] === imgCoord)[0];
   };
 
-  onClickBtn = (choice) => {
+  onClickBtn = (choice) => () => {
     const { imgCoord } = this.state;
     clearInterval(this.interval);
     console.log(imgCoord, choice);
@@ -82,7 +82,10 @@ class RockScissorsPaper extends Component {
         };
       });
     }
-    this.interval = setInterval(this.changeHand, 2000);
+
+    setTimeout(() => {
+      this.interval = setInterval(this.changeHand, 500);
+    }, 1000);
   };
 
   render() {
@@ -94,25 +97,17 @@ class RockScissorsPaper extends Component {
           style={{ background: `url(./rock.jpg) ${imgCoord} -90px` }}
         ></div>
         <div>
-          <button
-            id="rock"
-            className="btn"
-            onClick={() => this.onClickBtn("바위")}
-          >
+          <button id="rock" className="btn" onClick={this.onClickBtn("바위")}>
             바위
           </button>
           <button
             id="scissors"
             className="btn"
-            onClick={() => this.onClickBtn("가위")}
+            onClick={this.onClickBtn("가위")}
           >
             가위
           </button>
-          <button
-            id="paper"
-            className="btn"
-            onClick={() => this.onClickBtn("보")}
-          >
+          <button id="paper" className="btn" onClick={this.onClickBtn("보")}>
             보
           </button>
         </div>
