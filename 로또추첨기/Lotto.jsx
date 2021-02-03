6a9d2397c101e4d8af4df904521904dd96f7254c
Jiyon Lee <jiyonlee.d@gmail.com>
2021-02-03 19:06:40 +0900

    didupdate 추가 & 한번더 클릭시 함수구현

--- 로또추첨기/Lotto.jsx
+++ 로또추첨기/Lotto.jsx
@@ -33,8 +33,7 @@ class Lotto extends Component {
   };
 
   timeouts = [];
-
-  componentDidMount = () => {
+  runTimeouts = () => {
     const { winNum } = this.state;
     //let을 쓰면 클로저문제가 안생긴다.
     for (let i = 0; i < winNum.length - 1; i++) {
@@ -44,20 +43,43 @@ class Lotto extends Component {
             winBalls: [...prevState.winBalls, winNum[i]],
           };
         });
-      }, (i + 1) * 1000);
+      }, (i + 1) * 500);
     }
     this.timeouts[6] = setTimeout(() => {
       this.setState({
         bonus: winNum[winNum.length - 1],
         redo: true,
       });
-    }, 7000);
+    }, 3500);
+  };
+
+  componentDidMount = () => {
+    this.runTimeouts();
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    console.log("didUpdate");
+    //update에는 조건문이 중요하다, state가 바뀔 때마다 실행 (여기서는 볼 추가될때마다)
+    //조건문에 꼭 prev 안써도 된다.
+    if (this.state.winBalls.length === 0) {
+      this.runTimeouts();
+    }
   };
 
   componentWillUnmount = () => {
     this.timeouts.forEach((v) => {
       clearTimeout(v);
     });
+  };
+
+  onClickRedo = () => {
+    this.setState({
+      winNum: getNumbers(),
+      winBalls: [],
+      bonus: null,
+      redo: false,
+    });
+    this.timeouts = [];
   };
 
   render() {
